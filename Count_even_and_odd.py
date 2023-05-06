8f76e96e87f7adb6c6af16dec899ec562cd06f73
subin-007 <132741284+subin-007@users.noreply.github.com>
2023-05-06 21:39:59 +0530

    Update Count_even_and_odd.py
    added user input

--- Count_even_and_odd.py
+++ Count_even_and_odd.py
@@ -1,8 +1,18 @@
-sample_numbers = (2,8,9,45,67,4,10,5,34,56,99)
+sample_numbers = ()
+
+lst = list(sample_numbers)
+
 even_count = 0
 odd_count = 0
 
-for x in (sample_numbers):
+size = int(input("enter the total no.of elements"))
+
+for x in range (size):
+    element = int(input(f"enter the {x+1} element"))
+    lst.append(element)
+
+
+for x in (lst):
 
     if(x%2==0):
         even_count+=1
